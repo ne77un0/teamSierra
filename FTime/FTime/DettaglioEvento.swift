25a6ca7b58598cbbe70bc3af7d8ffb4aa564b3f2
mincolellinoemi <n.mincol@gmail.com>
2018-10-09 09:55:14 +0200

    add

---
+++ FTime/FTime/DettaglioEvento.swift
@@ -0,0 +1,30 @@
+//
+//  DettaglioEvento.swift
+//  FTime
+//
+//  Created by mincolellinoemi on 09/10/18.
+//  Copyright © 2018 ranaldogiovanni. All rights reserved.
+//
+
+import UIKit
+
+class DettaglioEvento: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
