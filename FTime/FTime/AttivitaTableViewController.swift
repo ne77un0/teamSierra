977fe694f3faa7c6acf1fed8e8acd9b817794588
mincolellinoemi <n.mincol@gmail.com>
2018-10-09 13:03:02 +0200

    label

--- FTime/FTime/AttivitaTableViewController.swift
+++ FTime/FTime/AttivitaTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AttivitaTableViewController: UITableViewController {
     
-    
+    var attivitaName =  ["Cinema", "Music", "Sport"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +26,33 @@ class AttivitaTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return attivitaName.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = attivitaName[indexPath.row]
 
         return cell
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if(segue.identifier == "segue"){
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let wc = segue.destination as! DettagliViewController
+                    wc.name = attivitaName[indexPath.row]
+            }
+        }
+    }
+    
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
---
+++ FTime/FTime/DettagliViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DettagliViewController.swift
+//  FTime
+//
+//  Created by mincolellinoemi on 09/10/18.
+//  Copyright © 2018 ranaldogiovanni. All rights reserved.
+//
+
+import UIKit
+
+class DettagliViewController: UIViewController {
+    var name: String?
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = name
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
