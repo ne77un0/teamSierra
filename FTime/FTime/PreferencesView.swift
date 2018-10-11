c96df8a40b272e3129af08481f9700d7a1c98dce
mincolellinoemi <n.mincol@gmail.com>
2018-10-11 12:27:34 +0200

    add profile add preferences

---
+++ FTime/FTime/PreferencesView.swift
@@ -0,0 +1,21 @@
+//
+//  PreferencesView.swift
+//  FTime
+//
+//  Created by mincolellinoemi on 11/10/18.
+//  Copyright © 2018 ranaldogiovanni. All rights reserved.
+//
+
+import UIKit
+
+class PreferencesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
